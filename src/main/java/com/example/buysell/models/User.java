6cc56705b4ba7d530474cbfb47e02f90ac2e9d56
KenArsen <kenjegulov2002@gmail.com>
2023-05-25 22:26:54 +0600

    Added OneToMany relation between User and Product

--- src/main/java/com/example/buysell/models/User.java
+++ src/main/java/com/example/buysell/models/User.java
@@ -7,9 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "users")
@@ -24,7 +22,7 @@ public class User implements UserDetails {
     @Column(name = "email", unique = true)
     private String email;
 
-    @Column(name = "phoneNumber", unique = true)
+    @Column(name = "phone_number")
     private String phoneNumber;
 
     @Column(name = "name")
@@ -44,6 +42,9 @@ public class User implements UserDetails {
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
+    private List<Product> products = new ArrayList<>();
 
     private LocalDateTime dateOfCreated;
 
