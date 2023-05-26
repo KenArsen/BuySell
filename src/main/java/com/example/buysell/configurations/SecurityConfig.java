22341e13cc5bc8cf657ef69c47c870b02a0cc0a8
KenArsen <kenjegulov2002@gmail.com>
2023-05-26 17:41:27 +0600

    Added admin panel

--- src/main/java/com/example/buysell/configurations/SecurityConfig.java
+++ src/main/java/com/example/buysell/configurations/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.buysell.services.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private final CustomUserDetailsService userDetailsService;
@@ -21,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/product/**", "/images/**", "/registration")
+                        .requestMatchers("/", "/product/**", "/images/**", "/registration", "/user/**")
                         .permitAll()
                         .anyRequest().authenticated()
                 )
